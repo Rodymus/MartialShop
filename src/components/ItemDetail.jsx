a7bd9ddf38543ef70a3589a63f019bcc6ee38499
Rodymus <61990609+Rodymus@users.noreply.github.com>
2023-03-09 20:07:44 -0300

    arreglando los container

---
+++ src/components/ItemDetail.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import { Image, Card, CardHeader, CardBody, CardFooter, Box, Stack, Heading, Divider } from "@chakra-ui/react";
+import { useParams } from 'react-router-dom';
+
+function ItemDetail() {const [products, setProducts] = useState([]);
+  const { category } = useParams();
+
+  useEffect(() => {
+    fetch('/src/data.json')
+      .then(response => response.json())
+      .then(data => setProducts(data.products));
+  }, []);
+
+  const filteredProducts = products.filter(product => product.category == category);
+    
+  return (
+    <div>
+      <h1>Producto</h1>
+      <ul>
+        {filteredProducts.map(product => (
+          <li key={products.category}>
+            <h2>{product.precio}</h2>
+            <Image maxW={{ base: '100%', sm: '150px' }} src={product.url}/>
+
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+  
+
+export default ItemDetail;
+
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,32 +1,11 @@
-import { useState, useEffect } from 'react';
-import { Image, Card, CardHeader, CardBody, CardFooter, Box, Stack, Heading, Divider } from "@chakra-ui/react";
-import { useParams } from 'react-router-dom';
+import ItemDetail from './ItemDetail';
 
-function ItemDetailContainer() {const [products, setProducts] = useState([]);
-  const { category } = useParams();
-
-  useEffect(() => {
-    fetch('/src/data.json')
-      .then(response => response.json())
-      .then(data => setProducts(data.products));
-  }, []);
-
-  const filteredProducts = products.filter(product => product.category == category);
-    
+const ItemDetailContainer = () => {
+     
   return (
-    <div>
-      <h1>Producto</h1>
-      <ul>
-        {filteredProducts.map(product => (
-          <li key={products.category}>
-            <h2>{product.precio}</h2>
-            <Image maxW={{ base: '100%', sm: '150px' }} src={product.url}/>
-
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ItemDetail />
   );
+
 }
   
 
